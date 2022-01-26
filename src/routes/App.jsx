3ed75f52675ea5a32bf60890f9e066e895eb246a
Yessenia <ymbeltran@gmail.com>
2022-01-26 11:32:13 -0500

    Files to start the project

---
+++ src/routes/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import '@styles/global.css';
+
+const App = () => {
+  return <div>Hola</div>;
+};
+
+export default App;
